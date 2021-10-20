5d0371e5fc1de2f4ec2107600b994443c17c1a09
thinhsu <92856141+thinhsu@users.noreply.github.com>
2021-10-20 17:07:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "HÔM NAY LÀ 20/10 ĐẤYYY NHÓ"",
-    introTitle: 'Nhé các bạn nữ xinh đẹp của lớp 11 Anh !',
+    titleWeb: "HÔM NAY LÀ 20/10 ĐẤYYY NHÓ",
+    introTitle: 'Nhé các bạn nữ xinh đẹp của lớp 11 Anh!',
     introDesc: `Trái đất tròn, lòng tôi góc cạnh
     Ngày bạn đến, lòng tôi bằng phẳng
     Trái đất hình cầu, nhưng 20/10 vẫn là ngày ở đỉnh chóp
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho 7 người đàn ông 11 chuyên Anh <3',
     reply: 'Các bạn con trai lớp mình tâm lý ghê á. Cảm ơn vì đã chúc bọn mình 20/10 nha. Các cậu xứng đáng 10 người yêu',
     mess: 'Bọn tao cảm ơn nha, biết là chúng mày sẽ nói thế mà hihi 😘😘',
-    messDesc: 'Nói vậy chứ, 20/10 phải thật vui vẻ nhé, thỏa sức mà chơi, bỏ lại deadlines và XÕA HẾT MÌNH đó nha!! Hôm nay là ngày của chúng mày. Chúng mày là những công chúa lộng lẫy nhất không chỉ hôm nay mà còn là 364 ngày còn lại nữa. Bấm Oke để xem những chân tình của các bạn nam nha:',
-    btnAccept: 'Okiiiii lun. Để tao chờ đợi xem chúng mày làm gì nha !',
+    messDesc: 'Nói vậy chứ, 20/10 phải thật vui vẻ nhé, thỏa sức mà chơi!Hôm nay là ngày của chúng mày. Chúng mày là những công chúa lộng lẫy nhất không chỉ hôm nay mà còn là 364 ngày còn lại nữa. Bấm Oke để xem tấm chân tình của các bạn nam nha',
+    btnAccept: 'Okiiiii lun. Để tao chờ đợi xem chúng mày làm gì nha!',
     messLink: 'https://youtu.be/8McC1UtG0H4' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
