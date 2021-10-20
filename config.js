52e27128bd80b7d7d417ba98aa62d91627e21fbc
thinhsu <92856141+thinhsu@users.noreply.github.com>
2021-10-20 16:55:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "HÔM NAY LÀ 20/10 ĐẤYYY"",
+    introTitle: 'Nhé các bạn nữ xinh đẹp của lớp 11 Anh !',
+    introDesc: `Trái đất tròn, lòng tôi góc cạnh
+    Ngày bạn đến, lòng tôi bằng phẳng
+    Trái đất hình cầu, nhưng 20/10 vẫn là ngày ở đỉnh chóp
+    Còn vị trí chúng mày thì luôn ở đỉnh TOPPPPP !`,
+    btnIntro: '^^AHiHi^^',
+    title: 'Chúc 20/10 chúng mày luôn xinh đẹp, vui tươi, cười nhiều hơn để xua đi nỗi buồn. Thi 9.0 IELTS, giải nhất HSG, 29 điểm ĐH. Sớm có người yêu :))))',
+    desc: 'Các cậu có thấy con trai lớp 11 Anh tâm lý không nào??',
+    btnYes: 'Vừa tâm lý lại còn đẹp trai, ga lăng, tuyệt vời nhất hệ mặt trời!',
+    btnNo: 'Không nha, ở đấy mà mơ. Con trai lớp Anh chẳng ga lăng tí nào cả :>',
+    question: 'Các cậu có muốn gửi cho 7 bạn con trai điều gì muốn nói không nè??',
+    btnReply: 'Gửi cho 7 người đàn ông 11 chuyên Anh <3',
+    reply: 'Các bạn con trai lớp mình tâm lý ghê á. Cảm ơn vì đã chúc bọn mình 20/10 nha. Các cậu xứng đáng 10 người yêu',
+    mess: 'Bọn tao cảm ơn nha, biết là chúng mày sẽ nói thế mà hihi 😘😘',
+    messDesc: 'Nói vậy chứ, 20/10 phải thật vui vẻ nhé, thỏa sức mà chơi, bỏ lại deadlines và XÕA HẾT MÌNH đó nha!! Hôm nay là ngày của chúng mày. Chúng mày là những công chúa lộng lẫy nhất không chỉ hôm nay mà còn là 364 ngày còn lại nữa. Bấm Oke để xem những chân tình của các bạn nam nha:',
+    btnAccept: 'Okiiiii lun. Để tao chờ đợi xem chúng mày làm gì nha !!!',
+    messLink: 'https://youtu.be/8McC1UtG0H4' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
